4d3eb77b4577348273924fc63795c35efba88ccb
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-03-20 21:35:56 -0400

    commit tree 13bb4eee0a6e74d4e0c262240316443e10d6a33f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code here!
+// const element = document.createElement("div")
+// document.body.append(element)
+
+// const ul = document.createElement("ul")
+
+// for (i=0; i < 3; i++) {
+//     const li = document.createElement("li")
+//     li.textContent = (i + 1).toString()
+//     ul.append(li)
+// }
+
+// element.append(ul)
+
+const mainId = document.getElementById("main")
+mainId.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML = "H1"
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
